95a6aed1cfb96604e1800578096756ccfc9cb5fe
KaosStorm <145504760+KaosStorm@users.noreply.github.com>
2023-11-14 15:30:08 -0800

    Not like a record, baby
    Ruby doesn't spin and neither do the objects.

--- Ruby'sAdventureCastillo/Assets/Scripts/RubyController.cs
+++ Ruby'sAdventureCastillo/Assets/Scripts/RubyController.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    Rigidbody2D rigidbody2D;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-     
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
 
+
+    }
+    private void FixedUpdate()
+    {
         Vector2 position = transform.position;
         position.x = position.x + 10.0f * horizontal * Time.deltaTime;
         position.y = position.y + 10.0f * vertical * Time.deltaTime;
 
-        transform.position = position;
+       rigidbody2D.MovePosition(position);
     }
 }
